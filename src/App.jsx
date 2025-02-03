5493447c14d50e19a4dab53914c8da20c3fb972a
devjohn42 <devjohncb@gmail.com>
2025-02-03 20:57:52 -0300

    login page | toasty

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Chat from './components/chat/chat'
 import Detail from './components/detail/detail'
 import List from './components/list/list'
+import Login from './components/login/login'
+import Notification from './components/notification/notification'
 
 const App = () => {
+  const user = true
+
   return (
     <div className="container">
-      <List />
-      <Chat />
-      <Detail />
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
     </div>
   )
 }
